d6c1d5b87b0548ae50ce114364539a42e5db526a
ss6987 <>
2018-09-17 23:52:48 +0900

    TANAKAZU更新分 ユーザー更新機能

--- src/servlet/UserUpdateServlet.java
+++ src/servlet/UserUpdateServlet.java
@@ -1,22 +1,25 @@
 package servlet;
 
 import Entity.User;
+import etc.ModelManager;
 
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
 
 public class UserUpdateServlet extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String disp = "/MainForward";
+        RequestDispatcher dispatch = request.getRequestDispatcher(disp);
         String userId = request.getParameter("user_id");
         String name = request.getParameter("name");
-        String phonetic =request.getParameter("phonetic");
-        String password =request.getParameter("password");
-        String salt = request.getParameter("salt");
+        String phonetic = request.getParameter("phonetic");
         Integer gender = Integer.parseInt(request.getParameter("gender"));
-        String birthday =  request.getParameter("birthday");
+        String birthday = request.getParameter("birthday");
         String postalCode = request.getParameter("postal_code");
         String address = request.getParameter("address");
         String tel = request.getParameter("tel");
@@ -34,7 +37,13 @@ public class UserUpdateServlet extends HttpServlet {
         errorString += user.setTel(tel);
         errorString += user.setUserClassification(userClassification);
 
+        ModelManager modelManager = new ModelManager();
+        boolean update = modelManager.userUpdate(user);
 
+        if (update != true) request.setAttribute("Error", "使用できない文章が存在します。");
+
+        request.setAttribute("Number", 11);
+        dispatch.forward(request, response);
     }
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
